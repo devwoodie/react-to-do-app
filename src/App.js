a20bdf47a08d94673b5d196ac6976e080b2afc83
devwoodie <ehddl453@naver.com>
2022-09-14 16:46:33 +0900

    feat : Form component 분리

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
 import List from './component/List';
+import Form from "./component/Form";
 import {useState} from "react";
 
 export default function App(){
 
     let [todoData, setTodoData] = useState([]);
-    let [value, setValue] = useState('');
+    let [value, setValue] = useState("");
 
-
-
-
-    const handleChange = e => {
-        setValue(e.target.value)
-    }
     const handleSubmit = e => {
         e.preventDefault();
 
@@ -25,32 +20,15 @@ export default function App(){
         setValue('');
     };
 
-
-
-        return(
-            <div className="container">
-                <div className="todoBlock">
-                    <div className="title">
-                        <h1>할 일 목록</h1>
-                    </div>
-                    <List todoData={todoData} setTodoData={setTodoData}/>
-                    <form style={{display : "flex"}} onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name="value"
-                            style={{ flex:"10", padding: "5px" }}
-                            placeholder="해야 할 일을 입력하세요."
-                            value={value}
-                            onChange={handleChange}
-                        />
-                        <input
-                            type="submit"
-                            value="입력"
-                            className="btn"
-                            style={{ flex: "1" }}
-                        />
-                    </form>
+    return(
+        <div className="container">
+            <div className="todoBlock">
+                <div className="title">
+                    <h1>할 일 목록</h1>
                 </div>
+                <List todoData={todoData} setTodoData={setTodoData}/>
+                <Form handleSubmit={handleSubmit} value={value} setValue={setValue}/>
             </div>
-        )
+        </div>
+    )
 }
