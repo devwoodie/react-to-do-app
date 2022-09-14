01f8fb17b0a59007ec52b91499c53c3087b39a81
devwoodie <ehddl453@naver.com>
2022-09-14 15:58:18 +0900

    feat : 함수형 컴포넌트로 바꾸기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import { Component } from 'react';
 import './App.css';
+import {useState} from "react";
 
-export default class App extends Component{
+export default function App(){
 
-    state = {
-        todoData : [],
-        value : ""
-    }
+    let [todoData, setTodoData] = useState([]);
+    let [value, setValue] = useState('');
 
-    btnStyle = {
+    const btnStyle = {
         color : "#fff",
         border : "none",
         padding : "5px 9px",
@@ -17,7 +15,7 @@ export default class App extends Component{
         float : "right"
     }
 
-    getStyle = (completed) => {
+    const getStyle = (completed) => {
         return {
             padding: "10px",
             borderBottom: "1px dotted #ccc",
@@ -25,36 +23,35 @@ export default class App extends Component{
         }
     }
 
-    handleClick = (id) => {
-        let newTodoData = this.state.todoData.filter(data => data.id !== id)
-        this.setState({todoData : newTodoData})
+    const handleClick = (id) => {
+        let newTodoData = todoData.filter(data => data.id !== id)
+        setTodoData(newTodoData)
     }
-    handleChange = e => {
-        this.setState({ value: e.target.value });
+    const handleChange = e => {
+        setValue(e.target.value)
     }
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
 
         let newTodo = {
             id: Date.now(),
-            title: this.state.value,
+            title: value,
             completed: false
         }
-
-        this.setState({ todoData : [...this.state.todoData, newTodo], value: "" })
+        setTodoData(prev => [...prev, newTodo]);
+        setValue('');
     };
 
-    handleCompleteChange = (id) => {
-        let newTodoData = this.state.todoData.map(data => {
+    const handleCompleteChange = (id) => {
+        let newTodoData = todoData.map(data => {
             if (data.id === id) {
                 data.completed = !data.completed;
             }
             return data;
         })
-        this.setState({ todoData : newTodoData })
+        setTodoData(newTodoData);
     }
 
-    render(){
         return(
             <div className="container">
                 <div className="todoBlock">
@@ -62,22 +59,22 @@ export default class App extends Component{
                         <h1>할 일 목록</h1>
                     </div>
                     {
-                        this.state.todoData.map((data) => (
-                            <div style={this.getStyle(data.completed)} key={data.id}>
-                                <input type="checkbox" defaultChecked={false} onChange={() => {this.handleCompleteChange(data.id)}}/>
+                        todoData.map((data) => (
+                            <div style={getStyle(data.completed)} key={data.id}>
+                                <input type="checkbox" defaultChecked={false} onChange={() => {handleCompleteChange(data.id)}}/>
                                 {data.title}
-                                <button style={this.btnStyle} onClick={() => {this.handleClick(data.id)}}>X</button>
+                                <button style={btnStyle} onClick={() => {handleClick(data.id)}}>X</button>
                             </div>
                         ))
                     }
-                    <form style={{display : "flex"}} onSubmit={this.handleSubmit}>
+                    <form style={{display : "flex"}} onSubmit={handleSubmit}>
                         <input
                             type="text"
                             name="value"
                             style={{ flex:"10", padding: "5px" }}
                             placeholder="해야 할 일을 입력하세요."
-                            value={this.state.value}
-                            onChange={this.handleChange}
+                            value={value}
+                            onChange={handleChange}
                         />
                         <input
                             type="submit"
@@ -89,5 +86,4 @@ export default class App extends Component{
                 </div>
             </div>
         )
-    }
 }
